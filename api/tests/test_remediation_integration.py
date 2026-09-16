56b60c27af7044b063fffa89aa604a7af01f57c4
Maria Violante <maria@mviolante.com>
2026-09-16 07:52:33 -0400

    fixed failing test

--- api/tests/test_remediation_integration.py
+++ api/tests/test_remediation_integration.py
@@ -26,6 +26,7 @@ from remediation.tests.factories import PdfUploadFactory
     TASKS={"default": {"BACKEND": "django.tasks.backends.immediate.ImmediateBackend"}},
     RUN_PRECHECK=False,
     RUN_OCR=False,
+    RUN_FONT_REPAIR=False,
     RUN_FINALIZE_METADATA=False,
     RUN_ALT_TEXT=False,
     RUN_LINK_TAG=False,
